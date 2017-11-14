e48dbabfa97f1d31577c0fc266f0a1dd707486fd
demickm <demickm@hotmail.com>
2017-11-13 18:14:24 -0700

    made changes to company properties names and keys file

--- USTrustEquipmentSeller/Models/CompanyProfile.swift
+++ USTrustEquipmentSeller/Models/CompanyProfile.swift
@@ -12,55 +12,55 @@ import UIKit
 struct CompanyProfile: FirebaseType {
 
     let companyName: String
-    var companyStreetAddress: String
-    var companyCity: String
-    var companyState: String
-    var companyZipCode: String
-    var companyPhone: String
-    var companyRating: Int
+    var streetAddress: String
+    var city: String
+    var state: String
+    var zipCode: String
+    var phone: String
+    var rating: Int
     var logo: UIImage
     var einNumber: String
     var administratorAccount: User
     var authorizedSellers: [User]
     var authorizedBuyers: [User]
-    var companyIsBanned: Bool
+    var isBanned: Bool
     var isVerified: Bool
     var endpoint: String = .companyEndpoint
     var identifier: String?
     var dictionaryCopy: [String : Any] {
         return [.companyNameKey: companyName,
-            .companyStreetAddressKey: companyStreetAddress,
-            .companyCityKey: companyCity,
-            .companyStateKey: companyState,
-            .companyZipCodeKey: companyZipCode,
-            .companyPhoneKey: companyPhone,
-            .companyRatingKey: companyRating,
+            .streetAddressKey: streetAddress,
+            .cityKey: city,
+            .stateKey: state,
+            .zipCodeKey: zipCode,
+            .phoneKey: phone,
+            .ratingKey: rating,
             .logoKey: logo,
             .einNumberKey: einNumber,
             .administratorAccountKey: administratorAccount,
             .authorizedSellersKey: authorizedSellers,
             .authorizedBuyersKey: authorizedBuyers,
-            .companyIsBannedKey: companyIsBanned,
+            .isBannedKey:isBanned,
             .isVerifiedKey: isVerified
             ]
     }
     
     
-    init(companyName: String, companyStreetAddress: String, companyCity: String, companyState: String, companyZipCode: String, companyPhone: String, companyRating: Int, logo: UIImage, einNumber: String, administratorAccount: User, authorizedSellers: [User], authorizedBuyers: [User], companyIsBanned: Bool = false, isVerified: Bool = false, identifier: String) {
+    init(companyName: String, streetAddress: String, city: String, state: String, zipCode: String, phone: String, rating: Int, logo: UIImage, einNumber: String, administratorAccount: User, authorizedSellers: [User], authorizedBuyers: [User], isBanned: Bool = false, isVerified: Bool = false, identifier: String) {
         
         self.companyName = companyName
-        self.companyStreetAddress = companyStreetAddress
-        self.companyCity = companyCity
-        self.companyState = companyState
-        self.companyZipCode = companyZipCode
-        self.companyPhone = companyPhone
-        self.companyRating = companyRating
+        self.streetAddress = streetAddress
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+        self.phone = phone
+        self.rating = rating
         self.logo = logo
         self.einNumber = einNumber
         self.administratorAccount = administratorAccount
         self.authorizedSellers = authorizedSellers
         self.authorizedBuyers = authorizedBuyers
-        self.companyIsBanned = companyIsBanned
+        self.isBanned = isBanned
         self.isVerified = isVerified
         self.identifier = identifier
         
@@ -68,35 +68,35 @@ struct CompanyProfile: FirebaseType {
     
     init?(dictionary: JSONDictionary, identifier: String) {
         guard let companyName = dictionary[.companyNameKey] as? String,
-            let companyStreetAddress = dictionary[.companyStreetAddressKey] as? String,
-            let companyCity = dictionary[.companyCityKey] as? String,
-            let companyState = dictionary[.companyStateKey] as? String,
-            let companyZipCode = dictionary[.companyZipCodeKey] as? String,
-            let companyPhone = dictionary[.companyPhoneKey] as? String,
-            let companyRating = dictionary[.companyRatingKey] as? Int,
+            let streetAddress = dictionary[.streetAddressKey] as? String,
+            let city = dictionary[.cityKey] as? String,
+            let state = dictionary[.stateKey] as? String,
+            let zipCode = dictionary[.zipCodeKey] as? String,
+            let phone = dictionary[.phoneKey] as? String,
+            let rating = dictionary[.ratingKey] as? Int,
             let logo = dictionary[.logoKey] as? UIImage,
             let einNumber = dictionary[.einNumberKey] as? String,
             let administratorAccount = dictionary[.administratorAccountKey] as? User,
             let authorizedSellers = dictionary[.authorizedSellersKey] as? [User],
             let authorizedBuyers = dictionary[.authorizedBuyersKey] as? [User],
-            let companyIsBanned = dictionary[.companyIsBannedKey] as? Bool,
+            let isBanned = dictionary[.isBannedKey] as? Bool,
             let isVerified = dictionary[.isVerifiedKey] as? Bool
             else { return nil }
         
         self.identifier = identifier
         self.companyName = companyName
-        self.companyStreetAddress = companyStreetAddress
-        self.companyCity = companyCity
-        self.companyState = companyState
-        self.companyZipCode = companyZipCode
-        self.companyPhone = companyPhone
-        self.companyRating = companyRating
+        self.streetAddress = streetAddress
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+        self.phone = phone
+        self.rating = rating
         self.logo = logo
         self.einNumber = einNumber
         self.administratorAccount = administratorAccount
         self.authorizedSellers = authorizedSellers
         self.authorizedBuyers = authorizedBuyers
-        self.companyIsBanned = companyIsBanned
+        self.isBanned = isBanned
         self.isVerified = isVerified
    
     }
--- USTrustEquipmentSeller/Resources/Helpers/Extensions/String+UES.swift
+++ USTrustEquipmentSeller/Resources/Helpers/Extensions/String+UES.swift
@@ -14,7 +14,7 @@ extension String {
     static var userEndpoint: String { get { return "user" } }
     static var companyEndpoint: String { get { return "companyProfile" } }
     
-    /* User Model Keys */
+    /*  Model Keys */
     static var firstNameKey: String { get { return "firstName" } }
     static var lastNameKey: String { get { return "lastName" } }
     static var streetAddressKey: String { get { return "streetAddress" } }
@@ -26,19 +26,13 @@ extension String {
     static var isBannedKey: String { get { return "isBanned" } }
     static var identifierKey: String { get { return "uid" } }
     
-    /* Company Model Keys */
     static var companyNameKey: String { get { return "companyName" } }
-    static var companyStreetAddressKey: String { get { return "companyStreetAddress" } }
-    static var companyCityKey: String { get { return "companyCity" } }
-    static var companyStateKey: String { get { return "companyState" } }
-    static var companyZipCodeKey: String { get { return "companyZipCode" } }
-    static var companyPhoneKey: String { get { return "companyPhone" } }
-    static var companyRatingKey: String { get { return "companyRating" } }
+    static var phoneKey: String { get { return "phone" } }
+    static var ratingKey: String { get { return "rating" } }
     static var logoKey: String { get { return "logo" } }
     static var einNumberKey: String { get { return "einNumber" } }
     static var administratorAccountKey: String { get { return "administratorAccount" } }
     static var authorizedSellersKey: String { get { return "authorizedSellers" } }
     static var authorizedBuyersKey: String { get { return "authorizedBuyers" } }
-    static var companyIsBannedKey: String { get { return "companyIsBanned" } }
     static var isVerifiedKey: String { get { return "isVerified" } }
 }
