ffce0a326d894904f1168892721e1476732034ce
demickm <demickm@hotmail.com>
2017-11-17 14:28:58 -0700

    Added listing table view cell, and search filter tableViewController and added view to browse listing storyboard

---
+++ USTrustEquipmentSeller/ViewControllers/ListingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListingTableViewCell.swift
+//  USTrustEquipmentSeller
+//
+//  Created by Demick McMullin on 11/17/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class ListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
