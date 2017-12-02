da7b5bf9e47568f3e3c10a7728535311d1085a5b
Demick McMullin <demickm@users.noreply.github.com>
2017-12-01 18:48:40 -0700

    D/develop/models (#5)
    * Beginning Storyboards setup
    
    * Added listing table view cell, and search filter tableViewController and added view to browse listing storyboard
    
    * A listing detail storyboard change
    
    * Added IBOutlets and Action to tapped buttons on Company Profile StoryBoard and ViewController.  No functions created yet
    
    * Changes made to Company Profile Storyboard, changed to tab bar controller, made changes to companyProfileViewController

--- USTrustEquipmentSeller/ViewControllers/CompanyProfileViewController.swift
+++ USTrustEquipmentSeller/ViewControllers/CompanyProfileViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class CompanyProfileViewController: UIViewController {
+class CompanyProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    /* Outlets */
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var companyName: UITextField!
+    @IBOutlet weak var streetAddress: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var einNumber: UITextField!
+    
+    /* Button Outlets */
+    @IBOutlet weak var saveEditButton: UIButton!
+    @IBOutlet weak var administratorSettingsButton: UIButton!
+    @IBOutlet weak var companyListingsButton: UIButton!
+    @IBOutlet weak var addImageButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if saveEditButton.titleLabel?.text != nil {
+            saveEditButton.titleLabel?.text = "Edit"
+        } else {
+            saveEditButton.titleLabel?.text = "Save"
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +52,72 @@ class CompanyProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func addImageButtonTapped(_ sender: Any) {
+        addPhotoActionSheet()
+    }
+    
+    
+    @IBAction func saveEditButtonTapped(_ sender: Any) {
+        saveEditButton.isEnabled = false
+        if saveEditButton.titleLabel?.text == "Edit" {
+           saveEditButton.titleLabel?.text = "Save"
+        } else {
+            saveEditButton.titleLabel?.text = "Edit"
+        }
+        
+    }
+    
+    func uploadButton() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func cameraButton() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .camera
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    func addPhotoActionSheet() {
+        let actionController = UIAlertController(title: "Upload Photo", message: nil, preferredStyle: .actionSheet)
+        let uploadAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            self.uploadButton()
+        }
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            self.cameraButton()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary)  &&  UIImagePickerController.isSourceTypeAvailable(.camera){
+            actionController.addAction(uploadAction)
+            actionController.addAction(cameraAction)
+        } else if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            actionController.addAction(cameraAction)
+        } else if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            actionController.addAction(uploadAction)
+        }
+        actionController.addAction(cancelAction)
+        present(actionController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        let size = CGSize(width: 600, height: 400)
+        let resizedImage = ImageHandler.resizeImage(image: selectedImage, targetSize: size )
+        
+        logoImage.image = resizedImage
+        
+        addImageButton.setTitle("", for: .normal)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
